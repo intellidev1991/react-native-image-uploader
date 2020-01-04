4c031015b1bab8656907895979edd3eefac29129
Hamed Taheri <hamed.taheri32@gmail.com>
2020-01-04 18:39:18 +0330

    init

---
+++ example/App.js
@@ -0,0 +1,31 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+/* eslint-disable */
+
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <SafeAreaView></SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
